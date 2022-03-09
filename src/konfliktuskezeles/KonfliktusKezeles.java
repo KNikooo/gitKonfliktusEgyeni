2f21654e31edfcfcf7c974026229cc0b5034df86
KNikooo <90379981+KNikooo@users.noreply.github.com>
2022-03-09 11:54:03 +0100

    3. nev dekra

--- src/konfliktuskezeles/KonfliktusKezeles.java
+++ src/konfliktuskezeles/KonfliktusKezeles.java
@@ -3,8 +3,10 @@ package konfliktuskezeles;
 public class KonfliktusKezeles {
 
     public static void main(String[] args) {
+        int i=7;
         String nev1="Niki";
         String nev2="Bibo";
+        int nev=3;
     }
     
 }
